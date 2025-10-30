87208d94bc8b30ea20222861a6c2e392b835401a
Fabrizio Scandola <collederas@gmail.com>
2025-10-30 13:25:12 +0100

    Fix tab navigation conflict for Inventory UI
    I added widget to override the Tab button config so that it will disable it on open: this avoids conflicts between engine trying to move focus and me wanting to close my inventory. this was also the reason OLT inventory key was changed to [i].

---
+++ HorrorSimRuntime/Public/UI/HorrorSimInventoryContainerWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/LyraActivatableWidget.h"
+#include "HorrorSimInventoryContainerWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORSIMRUNTIME_API UHorrorSimInventoryContainerWidget : public ULyraActivatableWidget
+{
+	GENERATED_BODY()
+};
